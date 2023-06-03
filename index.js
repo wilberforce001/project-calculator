a2576159a04fea3d39f651860f5fbc2dd0176452
wilberforce001 <kenmisik@gmail.com>
2023-06-03 20:38:23 +0300

    Grabbed all the variables using querry selectors and created global variables for the operator, previous and current value

---
+++ index.js
@@ -0,0 +1,19 @@
+let operator = '';
+let previousValue = '';
+let currentValue = '';
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    //Store all components on HTML and JS
+
+    let clear = document.querySelector("#clear-btn");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+})
